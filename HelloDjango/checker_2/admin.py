189e5e3818dffcf8250d6a6929555fa00d8a8dc7
vladiuse <vladiuse@gmail.com>
2022-07-29 23:08:43 +0300

    add CheckPoint Model

--- HelloDjango/checker_2/admin.py
+++ HelloDjango/checker_2/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import CheckPoint
 
-# Register your models here.
+
+class CheckPointAdmin(admin.ModelAdmin):
+    list_display = ['id', 'text','land_type', 'discount_type', 'for_geo', 'for_lang']
+
+
+admin.site.register(CheckPoint, CheckPointAdmin)
--- HelloDjango/checker_2/models.py
+++ HelloDjango/checker_2/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
 # Create your models here.
+
+class CheckPoint(models.Model):
+    LAND_TYPES = ('land', 'Лэндинг'), ('preland', 'Прэлендинг')
+    DISCOUNT_TYPE = ('free', 'Бесплатно'), ('low_price', 'Лоу-прайс')
+    text = models.CharField(max_length=255, verbose_name='Описание пункта проверки')
+    land_type = models.CharField(
+        max_length=20,
+        verbose_name='Тип сайта',
+        choices=LAND_TYPES,
+        null=True,
+        blank=True,
+    )
+
+    discount_type = models.CharField(
+        max_length=20,
+        verbose_name='Тип скидки',
+        choices=DISCOUNT_TYPE,
+        null=True,
+        blank=True,
+    )
+    for_geo = models.CharField(
+        max_length=255,
+        verbose_name='Только для гео',
+        blank=True,
+    )
+    for_lang = models.CharField(
+        max_length=255,
+        verbose_name='Только для языка',
+        blank=True,
+    )
+
+    class Meta:
+        verbose_name = 'Пункт проверки'
+        verbose_name_plural = 'Пункты проверки'
+
+    def __str__(self):
+        return self.text
