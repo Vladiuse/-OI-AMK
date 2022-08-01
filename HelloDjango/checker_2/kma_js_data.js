ca726b5ac94c34a8b49f4c594dc9eae18ee5f478
vladiuse <vladiuse@gmail.com>
2022-08-01 16:02:37 +0300

    add kma script

---
+++ HelloDjango/checker_2/kma_js_data.js
@@ -0,0 +1,30 @@
+tmp_data_to_server = "https://blog-feed.org/";
+country_list = {
+    ID: {
+        s1: 490000,
+        s2: 0,
+        s3: 490000,
+        s4: 980000,
+        discount: "50",
+        curr: "IDR",
+        specialfields: [],
+        rekv: "Netline LLC\u003Cbr\u003E129281, Moscow, ul. Izumrudnaya, d. 13, k. 2, et. 1, pom. 1, kom. 2, of. 4\u003Cbr\u003E",
+        campaign: "5121",
+    },
+};
+json_query = { ufl: "14926" };
+country = "ID";
+action_url = "https://trackerlead.biz/";
+request_id = "8b0701aa66f119c63240e96a46efb160";
+list_of_form_parameters =
+    '{"scenario":"normal_blog","language":"id","offer_id":"6829","transit":"14926","campaign":"5121","order_source":"2","return_url":"https://blog-feed.org/elle-breasty"}';
+city = "Brest";
+isJsonEnable = 0;
+text_item_is_free = "gratis";
+KMAText = {
+    validation_name: "Tentukan nama yang benar.",
+    validation_phone1: 'Nomor telepon hanya boleh berisi digit, "+", "-", "(", ")" dan spasi.',
+    validation_phone2: "Nomor telepon Anda terlalu kecil.",
+    validation_phone3: "В вашем телефоне слишком много цифр.",
+    comebacker_text: "PERINGATAN",
+};
