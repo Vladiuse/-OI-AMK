ed9b803df3d113e756f893756b2452df3fdfdd17
vladiuse <vladiuse@gmail.com>
2023-03-03 14:24:17 +0300

    add upper iso curensy display in admin

--- HelloDjango/kma/admin.py
+++ HelloDjango/kma/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.utils.translation import gettext_lazy as _
-from .models import DefaultWeb, Country, OfferPosition, UserApiKey, Language
+from .models import DefaultWeb, Country, OfferPosition, UserApiKey, Language, Currency
 
 
 class DefaultWebAdmin(admin.ModelAdmin):
@@ -12,11 +12,20 @@ class CountryAdmin(admin.ModelAdmin):
     list_display = ['iso', 'ru_full_name', 'phone', 'phone_code', 'currency', 'langs','iso3']
     list_display_links = ['iso', 'ru_full_name']
     search_fields = ['iso', 'ru_full_name']
-    autocomplete_fields = ['language']
+    autocomplete_fields = ['language', 'curr']
 
     list_filter = (
         ('phone', admin.EmptyFieldListFilter),
     )
+
+
+class CurrencyAdmin(admin.ModelAdmin):
+    list_display = ['upper_iso', 'name']
+    search_fields = ['iso', 'name']
+
+    @admin.display
+    def upper_iso(self, obj):
+        return obj.iso.upper()
 
 
 
@@ -38,4 +47,5 @@ admin.site.register(Country, CountryAdmin)
 admin.site.register(OfferPosition, OfferPositionAdmin)
 admin.site.register(UserApiKey)
 admin.site.register(Language, LanguageAdmin)
+admin.site.register(Currency,CurrencyAdmin)
 
