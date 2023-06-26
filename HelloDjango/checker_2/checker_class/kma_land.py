eb7cc2935a895b169ebed9a3fb6e2e725f814e30
vladiuse <vladiuse@gmail.com>
2023-06-26 13:17:51 +0300

    add default webs and new preland

--- HelloDjango/checker_2/checker_class/kma_land.py
+++ HelloDjango/checker_2/checker_class/kma_land.py
@@ -24,7 +24,7 @@ class KMALand(Land):
         'vim-store.ru',
     ]
     PRE_LAND_DOMAINS = ['blog-feed.org', 'blogs-info.info', 'previewpreland.pro', 'feed-news.org',
-                        'blogs-feed.org', 'feed-themes.org', 'feedz-newz.org', 'thirstday-news.org', 'monday-news.org', 'atirs-news.org'] + TEST_DOMAINS
+                        'blogs-feed.org', 'feed-themes.org', 'feedz-newz.org', 'thirstday-news.org', 'monday-news.org', 'atirs-news.org', 'calm-shop.org'] + TEST_DOMAINS
     INCORRECT_PRE_LAND_URLS = ['previewpreland.pro']
     NOT_WORKED_PRE_LAND_DOMAINS = ['blogs-info.info']
     MAIN_PRE_LAND_DOMAIN = 'blog-feed.org'
